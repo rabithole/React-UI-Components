3155b33283979122aeec3eba728dc8cf7d4d5614
Robert Kulp <pedalinon@gmail.com>
2019-05-09 13:52:07 -0700

    Header should be done

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Header.css';
 
-const Header = props => {
-	return <header className='header'>
-			<img src='https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png' alt='Lambda Logo'/>
-			<p><strong>Lambda School</strong> @LambdaSchool - 26 jan</p>
-		</header>;
-}
+import ImageThumbnail from './ImageThumbnail';
+// import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
-export default Header;
+const HeaderContainer = props => {
+	return <header className='header'>
+				<ImageThumbnail />
+				<HeaderContent />
+
+			</header>;
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = props => {
+	return <p className='headerTitle'><strong>Lambda School</strong> @LambdaSchool - 26 jan</p>;	
+};
+
+export default HeaderTitle;
