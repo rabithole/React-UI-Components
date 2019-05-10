94e7992f602ac353047046f4d70345616d9293d1
Robert Kulp <pedalinon@gmail.com>
2019-05-10 09:59:49 -0700

    Conatainers built

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-// import Container from './components/CardComponents/CardContainer';
+import Container from './components/CardComponents/CardContainer';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div className='mainDiv'>
-      <HeaderContainer />
-      
-     
+      	<HeaderContainer />
+      	<Container />
+    	<Footer />
     </div>
   );
 };
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = props => {
+	return <footer>Something will go here</footer>
+}
+
+export default Footer;
