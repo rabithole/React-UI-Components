d96f1c7d87264ce84909903cb74ac3640d0de81c
Robert Kulp <pedalinon@gmail.com>
2019-05-12 09:58:53 -0700

    Falling together

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -7,26 +7,22 @@ const App = () => {
   return (
     <div className='mainDiv'>
       <CalculatorDisplay />
+      <NumberButton buttonStyle='NumberButton longButton' text='clear'/>
+      <NumberButton buttonStyle='NumberButton actionButton' text='/'/>
+      <NumberButton buttonStyle='NumberButton' text='7'/>
+      <NumberButton buttonStyle='NumberButton' text='8'/>
+      <NumberButton buttonStyle='NumberButton' text='9'/>
+      <NumberButton buttonStyle='NumberButton actionButton' text='X'/>
+      <NumberButton buttonStyle='NumberButton' text='4'/>
+      <NumberButton buttonStyle='NumberButton' text='5'/>
+      <NumberButton buttonStyle='NumberButton' text='6'/>
+      <NumberButton buttonStyle='NumberButton actionButton' text='-'/>
       <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
-      <NumberButton buttonStyle='NumberButton' text='1'/>
+      <NumberButton buttonStyle='NumberButton' text='2'/>
+      <NumberButton buttonStyle='NumberButton' text='3'/>
+      <NumberButton buttonStyle='NumberButton actionButton' text='+'/>
+      <NumberButton buttonStyle='NumberButton longButton' text='0'/>
+      <NumberButton buttonStyle='NumberButton actionButton' text='='/>
     </div>
   );
 };
